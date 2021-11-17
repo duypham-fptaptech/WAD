8c0833fa374bfad402d4fa2261d8937d4d59d907
duypham-fptaptech <duypdth2009006@fpt.edu.vn>
2021-11-17 16:16:49 +0700

    Pratice

---
+++ PraticeWad/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PraticeWad.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        [Required]
+        public string EmployeeId { get; set; }
+        [Required]
+        public string EmployeeName { get; set; }
+        [Required]
+        public string Department { get; set; }
+        [Required]
+        public int Salary { get; set; }
+    }
+}
